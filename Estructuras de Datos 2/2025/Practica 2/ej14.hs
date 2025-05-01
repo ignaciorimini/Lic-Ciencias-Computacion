ad91f47c690fb76fafdef1be8d715e1f4ffb31b0
Ignacio Rimini <90485317+ignaciorimini@users.noreply.github.com>
2025-05-01 18:02:55 -0300

    Delete Estructuras de Datos 2 directory

--- Estructuras de Datos 2/2025/Practica 2/ej14.hs
+++
@@ -1,5 +0,0 @@
--- Producto escalar de dos listas de enteros de igual longitud: suma de los productos delos elementos sucesivos (misma posición) de ambas listas.
--- scalarProduct [1, 2, 3] [4, 5, 6] -> (1*4 + 2*5 + 3*6) = 4 + 10 + 18 = 32
-
-scalarProduct :: [Int] -> [Int] -> Int
-scalarProduct xs ys = sum [a*b | (a,b) <- zip xs ys]
